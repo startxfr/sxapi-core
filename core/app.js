bfad375e4fb3abb2df19d07cba186078b1677285
startxfr <clarue@startx.fr>
2016-11-09 11:41:19 +0100

    testing

--- core/app.js
+++ core/app.js
@@ -1,4 +1,4 @@
-/* global module, require, process */
+/* global module, require, process, chris */
 
 // declaring global variable $timer 
 $timer = require('./timer');
@@ -14,6 +14,7 @@ var $app = {
         network_port: 8080
     },
     config: {
+        test:'dsdsd',
         ip: require("ip").address(),
         log: {}
     },
