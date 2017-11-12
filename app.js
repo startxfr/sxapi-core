02a5bfbf80ff881270d786c51f027f6b571c2b60
startxfr <clarue@startx.fr>
2017-11-12 13:37:57 +0100

    Remove test and publish app for npm module

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 /* global require, process, $log, $timer */
 
-exports.printMsg = function() {
-	  console.log("This is a message from the demo package");
-}
-//exports = require("./core/app");
+exports.timer= $timer;
+exports.log = $log;
+exports.app = require("./core/app");
 
 //$app.launch(function () {
 //    $log.info("application started", $timer.time('app'));
