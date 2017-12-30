c10f74243b68406b259951c94368078b562eb6fa
startxfr <clarue@startx.fr>
2017-12-30 17:50:30 +0100

    enable simple and full mode for testing env

---
+++ test/mock_data/mysql/data.sql
@@ -0,0 +1,30 @@
+SET names 'utf8';
+-- MySQL dump 10.14  Distrib 5.5.57-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: db-mysql    Database: startx-db-tools
+-- ------------------------------------------------------
+-- Server version	10.1.22-MariaDB-1~jessie
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `app`
+--
+LOCK TABLES `app` WRITE;
+INSERT INTO `app` VALUES 
+(1,'version','0.0.22');
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'startx-db-tools'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-12-26 23:27:52
