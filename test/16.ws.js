5e1644cd3d89b712db375dee46e3de34d9ca63f1
startxfr <clarue@startx.fr>
2018-01-29 00:52:19 +0100

    change test format

--- test/16.ws.js
+++ test/16.ws.js
@@ -12,118 +12,118 @@ var test = null;
 
 /* Test suite for ws library */
 describe("Resource core component", function () {
-  describe("ws object", function () {
-    test = require("../core/ws");
-    it("should be an object", function () {
-      expect(test).to.be.an('object');
+    describe("ws object", function () {
+        test = require("../core/ws");
+        it("should be an object", function () {
+            expect(test).to.be.an('object');
+        });
+        it("should have config property", function () {
+            expect(test).to.have.any.keys('config');
+            expect(test.config).to.be.an('object');
+        });
     });
-    it("should have config property", function () {
-      expect(test).to.have.any.keys('config');
-      expect(test.config).to.be.an('object');
+    describe("#init()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('init');
+        });
+        it("should be a method", function () {
+            expect(test.init).to.be.an('function');
+        });
+        describe("#_initApp()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('_initApp');
+            });
+            it("should be a method", function () {
+                expect(test._initApp).to.be.an('function');
+            });
+        });
+        describe("#_initEndpoints()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('_initEndpoints');
+            });
+            it("should be a method", function () {
+                expect(test._initEndpoints).to.be.an('function');
+            });
+        });
+        describe("#_initEndpoint()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('_initEndpoint');
+            });
+            it("should be a method", function () {
+                expect(test._initEndpoint).to.be.an('function');
+            });
+        });
+        describe("#_initEndpointConfig()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('_initEndpointConfig');
+            });
+            it("should be a method", function () {
+                expect(test._initEndpointConfig).to.be.an('function');
+            });
+        });
+        describe("#__defaultEndpointCb()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('__defaultEndpointCb');
+            });
+            it("should be a method", function () {
+                expect(test.__defaultEndpointCb).to.be.an('function');
+            });
+        });
     });
-  });
-  describe("#init()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('init');
+    describe("#start()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('start');
+        });
+        it("should be a method", function () {
+            expect(test.start).to.be.an('function');
+        });
     });
-    it("should be a method", function () {
-      expect(test.init).to.be.an('function');
+    describe("#stop()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('stop');
+        });
+        it("should be a method", function () {
+            expect(test.stop).to.be.an('function');
+        });
     });
-    describe("#_initApp()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('_initApp');
-      });
-      it("should be a method", function () {
-        expect(test._initApp).to.be.an('function');
-      });
+    describe("#okResponse()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('okResponse');
+        });
+        it("should be a method", function () {
+            expect(test.okResponse).to.be.an('function');
+        });
     });
-    describe("#_initEndpoints()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('_initEndpoints');
-      });
-      it("should be a method", function () {
-        expect(test._initEndpoints).to.be.an('function');
-      });
+    describe("#nokResponse()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('nokResponse');
+        });
+        it("should be a method", function () {
+            expect(test.nokResponse).to.be.an('function');
+        });
     });
-    describe("#_initEndpoint()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('_initEndpoint');
-      });
-      it("should be a method", function () {
-        expect(test._initEndpoint).to.be.an('function');
-      });
+    describe("#response()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('response');
+        });
+        it("should be a method", function () {
+            expect(test.response).to.be.an('function');
+        });
     });
-    describe("#_initEndpointConfig()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('_initEndpointConfig');
-      });
-      it("should be a method", function () {
-        expect(test._initEndpointConfig).to.be.an('function');
-      });
+    describe("#defaultRouter()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('defaultRouter');
+        });
+        it("should be a method", function () {
+            expect(test.defaultRouter).to.be.an('function');
+        });
     });
-    describe("#__defaultEndpointCb()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('__defaultEndpointCb');
-      });
-      it("should be a method", function () {
-        expect(test.__defaultEndpointCb).to.be.an('function');
-      });
+    describe("#dynamicRequestHandlerTest()", function () {
+        it("should exist", function () {
+            expect(test).to.have.any.keys('dynamicRequestHandlerTest');
+        });
+        it("should be a method", function () {
+            expect(test.dynamicRequestHandlerTest).to.be.an('function');
+        });
     });
-  });
-  describe("#start()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('start');
-    });
-    it("should be a method", function () {
-      expect(test.start).to.be.an('function');
-    });
-  });
-  describe("#stop()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('stop');
-    });
-    it("should be a method", function () {
-      expect(test.stop).to.be.an('function');
-    });
-  });
-  describe("#okResponse()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('okResponse');
-    });
-    it("should be a method", function () {
-      expect(test.okResponse).to.be.an('function');
-    });
-  });
-  describe("#nokResponse()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('nokResponse');
-    });
-    it("should be a method", function () {
-      expect(test.nokResponse).to.be.an('function');
-    });
-  });
-  describe("#response()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('response');
-    });
-    it("should be a method", function () {
-      expect(test.response).to.be.an('function');
-    });
-  });
-  describe("#defaultRouter()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('defaultRouter');
-    });
-    it("should be a method", function () {
-      expect(test.defaultRouter).to.be.an('function');
-    });
-  });
-  describe("#dynamicRequestHandlerTest()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('dynamicRequestHandlerTest');
-    });
-    it("should be a method", function () {
-      expect(test.dynamicRequestHandlerTest).to.be.an('function');
-    });
-  });
 });
--- test/33.resource-postgres.js
+++ test/33.resource-postgres.js
@@ -6,250 +6,250 @@ var test = null;
 
 /* Test suite for postgres resource */
 describe("postgres resource component", function () {
-  describe("app object", function () {
-    test = require("../core/resource/postgres");
-    test = test("test-postgres", {
-      server: {
-        "host": "172.17.42.1",
-        "database": "test"
-      }
+    describe("app object", function () {
+        test = require("../core/resource/postgres");
+        test = test("test-postgres", {
+            server: {
+                "host": "172.17.42.1",
+                "database": "test"
+            }
+        });
+        it("should be an object", function () {
+            expect(test).to.be.an('object');
+        });
+        it("should have config property", function () {
+            expect(test).to.have.any.keys('config');
+            expect(test.config).to.be.an('object');
+        });
+        it("should have id property", function () {
+            expect(test).to.have.any.keys('id');
+            expect(test.id).to.be.an('string');
+        });
+        it("should have endpoints property", function () {
+            expect(test).to.have.any.keys('endpoints');
+            expect(test.endpoints).to.be.an('object');
+        });
+        it("should have tools property", function () {
+            expect(test).to.have.any.keys('tools');
+            expect(test.tools).to.be.an('object');
+        });
+        describe("#tools", function () {
+            describe("#generateParams4Template()", function () {
+                it("should exist", function () {
+                    expect(test.tools).to.have.any.keys('generateParams4Template');
+                });
+                it("should be a method", function () {
+                    expect(test.tools.generateParams4Template).to.be.an('function');
+                });
+            });
+            describe("#responseResourceDoesntExist()", function () {
+                it("should exist", function () {
+                    expect(test.tools).to.have.any.keys('responseResourceDoesntExist');
+                });
+                it("should be a method", function () {
+                    expect(test.tools.responseResourceDoesntExist).to.be.an('function');
+                });
+            });
+            describe("#responseOK()", function () {
+                it("should exist", function () {
+                    expect(test.tools).to.have.any.keys('responseOK');
+                });
+                it("should be a method", function () {
+                    expect(test.tools.responseOK).to.be.an('function');
+                });
+            });
+            describe("#responseNOK()", function () {
+                it("should exist", function () {
+                    expect(test.tools).to.have.any.keys('responseNOK');
+                });
+                it("should be a method", function () {
+                    expect(test.tools.responseNOK).to.be.an('function');
+                });
+            });
+            describe("#format()", function () {
+                it("should exist", function () {
+                    expect(test.tools).to.have.any.keys('format');
+                });
+                it("should be a method", function () {
+                    expect(test.tools.format).to.be.an('function');
+                });
+            });
+        });
     });
-    it("should be an object", function () {
-      expect(test).to.be.an('object');
-    });
-    it("should have config property", function () {
-      expect(test).to.have.any.keys('config');
-      expect(test.config).to.be.an('object');
-    });
-    it("should have id property", function () {
-      expect(test).to.have.any.keys('id');
-      expect(test.id).to.be.an('string');
-    });
-    it("should have endpoints property", function () {
-      expect(test).to.have.any.keys('endpoints');
-      expect(test.endpoints).to.be.an('object');
-    });
-    it("should have tools property", function () {
-      expect(test).to.have.any.keys('tools');
-      expect(test.tools).to.be.an('object');
-    });
-    describe("#tools", function () {
-      describe("#generateParams4Template()", function () {
+    describe("#init()", function () {
         it("should exist", function () {
-          expect(test.tools).to.have.any.keys('generateParams4Template');
+            expect(test).to.have.any.keys('init');
         });
         it("should be a method", function () {
-          expect(test.tools.generateParams4Template).to.be.an('function');
+            expect(test.init).to.be.an('function');
         });
-      });
-      describe("#responseResourceDoesntExist()", function () {
+    });
+    describe("#start()", function () {
         it("should exist", function () {
-          expect(test.tools).to.have.any.keys('responseResourceDoesntExist');
+            expect(test).to.have.any.keys('start');
         });
         it("should be a method", function () {
-          expect(test.tools.responseResourceDoesntExist).to.be.an('function');
+            expect(test.start).to.be.an('function');
         });
-      });
-      describe("#responseOK()", function () {
+    });
+    describe("#stop()", function () {
         it("should exist", function () {
-          expect(test.tools).to.have.any.keys('responseOK');
+            expect(test).to.have.any.keys('stop');
         });
         it("should be a method", function () {
-          expect(test.tools.responseOK).to.be.an('function');
+            expect(test.stop).to.be.an('function');
         });
-      });
-      describe("#responseNOK()", function () {
+    });
+    describe("#open()", function () {
         it("should exist", function () {
-          expect(test.tools).to.have.any.keys('responseNOK');
+            expect(test).to.have.any.keys('open');
         });
         it("should be a method", function () {
-          expect(test.tools.responseNOK).to.be.an('function');
+            expect(test.open).to.be.an('function');
         });
-      });
-      describe("#format()", function () {
-        it("should exist", function () {
-          expect(test.tools).to.have.any.keys('format');
+    });
+    describe("resource methods", function () {
+        describe("#query()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('query');
+            });
+            it("should be a method", function () {
+                expect(test.query).to.be.an('function');
+            });
+            describe("#__queryDefaultCallback()", function () {
+                it("should exist", function () {
+                    expect(test).to.have.any.keys('__queryDefaultCallback');
+                });
+                it("should be a method", function () {
+                    expect(test.__queryDefaultCallback).to.be.an('function');
+                });
+            });
         });
-        it("should be a method", function () {
-          expect(test.tools.format).to.be.an('function');
+        describe("#read()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('read');
+            });
+            it("should be a method", function () {
+                expect(test.read).to.be.an('function');
+            });
+            describe("#__readDefaultCallback()", function () {
+                it("should exist", function () {
+                    expect(test).to.have.any.keys('__readDefaultCallback');
+                });
+                it("should be a method", function () {
+                    expect(test.__readDefaultCallback).to.be.an('function');
+                });
+            });
         });
-      });
-    });
-  });
-  describe("#init()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('init');
-    });
-    it("should be a method", function () {
-      expect(test.init).to.be.an('function');
-    });
-  });
-  describe("#start()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('start');
-    });
-    it("should be a method", function () {
-      expect(test.start).to.be.an('function');
-    });
-  });
-  describe("#stop()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('stop');
-    });
-    it("should be a method", function () {
-      expect(test.stop).to.be.an('function');
-    });
-  });
-  describe("#open()", function () {
-    it("should exist", function () {
-      expect(test).to.have.any.keys('open');
-    });
-    it("should be a method", function () {
-      expect(test.open).to.be.an('function');
-    });
-  });
-  describe("resource methods", function () {
-    describe("#query()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('query');
-      });
-      it("should be a method", function () {
-        expect(test.query).to.be.an('function');
-      });
-      describe("#__queryDefaultCallback()", function () {
-        it("should exist", function () {
-          expect(test).to.have.any.keys('__queryDefaultCallback');
+        describe("#insert()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('insert');
+            });
+            it("should be a method", function () {
+                expect(test.insert).to.be.an('function');
+            });
+            describe("#__insertDefaultCallback()", function () {
+                it("should exist", function () {
+                    expect(test).to.have.any.keys('__insertDefaultCallback');
+                });
+                it("should be a method", function () {
+                    expect(test.__insertDefaultCallback).to.be.an('function');
+                });
+            });
         });
-        it("should be a method", function () {
-          expect(test.__queryDefaultCallback).to.be.an('function');
+        describe("#update()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('update');
+            });
+            it("should be a method", function () {
+                expect(test.update).to.be.an('function');
+            });
+            describe("#__updateDefaultCallback()", function () {
+                it("should exist", function () {
+                    expect(test).to.have.any.keys('__updateDefaultCallback');
+                });
+                it("should be a method", function () {
+                    expect(test.__updateDefaultCallback).to.be.an('function');
+                });
+            });
         });
-      });
-    });
-    describe("#read()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('read');
-      });
-      it("should be a method", function () {
-        expect(test.read).to.be.an('function');
-      });
-      describe("#__readDefaultCallback()", function () {
-        it("should exist", function () {
-          expect(test).to.have.any.keys('__readDefaultCallback');
+        describe("#delete()", function () {
+            it("should exist", function () {
+                expect(test).to.have.any.keys('delete');
+            });
+            it("should be a method", function () {
+                expect(test.delete).to.be.an('function');
+            });
+            describe("#__deleteDefaultCallback()", function () {
+                it("should exist", function () {
+                    expect(test).to.have.any.keys('__deleteDefaultCallback');
+                });
+                it("should be a method", function () {
+                    expect(test.__deleteDefaultCallback).to.be.an('function');
+                });
+            });
         });
-        it("should be a method", function () {
-          expect(test.__readDefaultCallback).to.be.an('function');
+    });
+    describe("resource endpoints", function () {
+        describe("#list()", function () {
+            it("should exist", function () {
+                expect(test.endpoints).to.have.any.keys('list');
+            });
+            it("should be a method", function () {
+                expect(test.endpoints.list).to.be.an('function');
+            });
+            var result = test.endpoints.list({});
+            it("should return a webserver callback", function () {
+                expect(result).to.be.an('function');
+            });
         });
-      });
-    });
-    describe("#insert()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('insert');
-      });
-      it("should be a method", function () {
-        expect(test.insert).to.be.an('function');
-      });
-      describe("#__insertDefaultCallback()", function () {
-        it("should exist", function () {
-          expect(test).to.have.any.keys('__insertDefaultCallback');
+        describe("#read()", function () {
+            it("should exist", function () {
+                expect(test.endpoints).to.have.any.keys('read');
+            });
+            it("should be a method", function () {
+                expect(test.endpoints.read).to.be.an('function');
+            });
+            var result = test.endpoints.read({});
+            it("should return a webserver callback", function () {
+                expect(result).to.be.an('function');
+            });
         });
-        it("should be a method", function () {
-          expect(test.__insertDefaultCallback).to.be.an('function');
+        describe("#create()", function () {
+            it("should exist", function () {
+                expect(test.endpoints).to.have.any.keys('create');
+            });
+            it("should be a method", function () {
+                expect(test.endpoints.create).to.be.an('function');
+            });
+            var result = test.endpoints.create({});
+            it("should return a webserver callback", function () {
+                expect(result).to.be.an('function');
+            });
         });
-      });
-    });
-    describe("#update()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('update');
-      });
-      it("should be a method", function () {
-        expect(test.update).to.be.an('function');
-      });
-      describe("#__updateDefaultCallback()", function () {
-        it("should exist", function () {
-          expect(test).to.have.any.keys('__updateDefaultCallback');
+        describe("#update()", function () {
+            it("should exist", function () {
+                expect(test.endpoints).to.have.any.keys('update');
+            });
+            it("should be a method", function () {
+                expect(test.endpoints.update).to.be.an('function');
+            });
+            var result = test.endpoints.update({});
+            it("should return a webserver callback", function () {
+                expect(result).to.be.an('function');
+            });
         });
-        it("should be a method", function () {
-          expect(test.__updateDefaultCallback).to.be.an('function');
+        describe("#delete()", function () {
+            it("should exist", function () {
+                expect(test.endpoints).to.have.any.keys('delete');
+            });
+            it("should be a method", function () {
+                expect(test.endpoints.delete).to.be.an('function');
+            });
+            var result = test.endpoints.delete({});
+            it("should return a webserver callback", function () {
+                expect(result).to.be.an('function');
+            });
         });
-      });
     });
-    describe("#delete()", function () {
-      it("should exist", function () {
-        expect(test).to.have.any.keys('delete');
-      });
-      it("should be a method", function () {
-        expect(test.delete).to.be.an('function');
-      });
-      describe("#__deleteDefaultCallback()", function () {
-        it("should exist", function () {
-          expect(test).to.have.any.keys('__deleteDefaultCallback');
-        });
-        it("should be a method", function () {
-          expect(test.__deleteDefaultCallback).to.be.an('function');
-        });
-      });
-    });
-  });
-  describe("resource endpoints", function () {
-    describe("#list()", function () {
-      it("should exist", function () {
-        expect(test.endpoints).to.have.any.keys('list');
-      });
-      it("should be a method", function () {
-        expect(test.endpoints.list).to.be.an('function');
-      });
-      var result = test.endpoints.list({});
-      it("should return a webserver callback", function () {
-        expect(result).to.be.an('function');
-      });
-    });
-    describe("#read()", function () {
-      it("should exist", function () {
-        expect(test.endpoints).to.have.any.keys('read');
-      });
-      it("should be a method", function () {
-        expect(test.endpoints.read).to.be.an('function');
-      });
-      var result = test.endpoints.read({});
-      it("should return a webserver callback", function () {
-        expect(result).to.be.an('function');
-      });
-    });
-    describe("#create()", function () {
-      it("should exist", function () {
-        expect(test.endpoints).to.have.any.keys('create');
-      });
-      it("should be a method", function () {
-        expect(test.endpoints.create).to.be.an('function');
-      });
-      var result = test.endpoints.create({});
-      it("should return a webserver callback", function () {
-        expect(result).to.be.an('function');
-      });
-    });
-    describe("#update()", function () {
-      it("should exist", function () {
-        expect(test.endpoints).to.have.any.keys('update');
-      });
-      it("should be a method", function () {
-        expect(test.endpoints.update).to.be.an('function');
-      });
-      var result = test.endpoints.update({});
-      it("should return a webserver callback", function () {
-        expect(result).to.be.an('function');
-      });
-    });
-    describe("#delete()", function () {
-      it("should exist", function () {
-        expect(test.endpoints).to.have.any.keys('delete');
-      });
-      it("should be a method", function () {
-        expect(test.endpoints.delete).to.be.an('function');
-      });
-      var result = test.endpoints.delete({});
-      it("should return a webserver callback", function () {
-        expect(result).to.be.an('function');
-      });
-    });
-  });
 });
