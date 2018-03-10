4f0ed7922f554e9d7ccbf4bbac2b5362d653b3f6
startxfr <clarue@startx.fr>
2018-03-10 15:14:18 +0100

    add templating of various params in config file

--- core/resource/couchbase.js
+++ core/resource/couchbase.js
@@ -27,6 +27,14 @@ module.exports = function (id, config) {
       if (!$cbdb.config.bucket) {
         throw new Error("no 'bucket' key found in resource '" + $cbdb.id + "' config");
       }
+      $cbdb.config.cluster = $log.format($cbdb.config.cluster, process.env);
+      $cbdb.config.bucket = $log.format($cbdb.config.bucket, process.env);
+      if ($cbdb.config.user) {
+        $cbdb.config.user = $log.format($cbdb.config.user, process.env);
+      }
+      if ($cbdb.config.password) {
+        $cbdb.config.password = $log.format($cbdb.config.password, process.env);
+      }
       $cbdb.cb = require("couchbase");
       if (typeof $cbdb.pool[$cbdb.config.cluster] === 'undefined') {
         $log.tools.resourceDebug($cbdb.id, "open new connection to cluster " + $cbdb.config.cluster, 4);
